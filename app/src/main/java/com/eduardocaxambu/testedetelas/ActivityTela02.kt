58da4671fc6b26ed99787cfbd30019e5d75f0b3d
Eduardo Caxambu <ejocx@hotmail.com>
2022-08-19 09:06:31 -0300

    Initial commit

---
+++ app/src/main/java/com/eduardocaxambu/testedetelas/ActivityTela02.kt
@@ -0,0 +1,11 @@
+package com.eduardocaxambu.testedetelas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityTela02 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela02)
+    }
+}
